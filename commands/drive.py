5d8f262b1804018e2ddc93b60af5bf0c6b22e7c6
PickleFace5 <caden.dalley@icloud.com>
2024-01-15 20:00:23 -0800

    Teleop Improvements

--- commands/drive.py
+++ commands/drive.py
@@ -1,11 +1,15 @@
-from commands2 import CommandBase
+from commands2 import Command
 from constants import *
 from frc6343.controller.deadband import deadband
 from subsystems.swerve import Swerve
 from wpilib import SmartDashboard, XboxController
+from wpimath.filter import SlewRateLimiter
 from wpimath.kinematics import ChassisSpeeds
 
-class DriveByController(CommandBase):
+class DriveByController(Command):
+    transXSlew = SlewRateLimiter(0.5)
+    transYSlew = SlewRateLimiter(0.5)
+    rotSlew = SlewRateLimiter(0.5)
 
     def __init__(self, swerve: Swerve, controller: XboxController) -> None:
         super().__init__()
@@ -16,16 +20,16 @@ class DriveByController(CommandBase):
         self.controller = controller
 
     def initialize(self) -> None:
-        self.swerve.initialize()
+        self.swerve.initialize()        
     
     def execute(self) -> None:
         translationX = -self.controller.getLeftY()
         translationY = -self.controller.getLeftX()
         rotation = -self.controller.getRightX()
 
-        translationY = deadband(translationY, DriverController.deadband)
-        translationX = deadband(translationX, DriverController.deadband)
-        rotation = deadband(rotation, DriverController.deadband)
+        translationY = self.transXSlew.calculate(deadband(translationY, DriverController.deadband) ** 3)
+        translationX = self.transYSlew.calculate(deadband(translationX, DriverController.deadband) ** 3)
+        rotation = self.rotSlew.calculate(deadband(rotation, DriverController.deadband) ** 3)
 
         self.swerve.drive(ChassisSpeeds(translationX * Larry.kMaxSpeed, translationY * Larry.kMaxSpeed, rotation * Larry.kMaxRotRate), fieldRelative=True)
     
