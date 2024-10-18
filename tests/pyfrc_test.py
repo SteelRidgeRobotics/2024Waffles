d938cfeb13342e66418e3830052cfd0eed6c0503
PickleFace5 <caden.dalley@icloud.com>
2024-10-18 00:09:25 -0700

    Optimized robotpy tests

--- tests/pyfrc_test.py
+++ tests/pyfrc_test.py
@@ -3,4 +3,21 @@
     to test basic functionality of just about any robot.
 '''
 
-from pyfrc.tests import *
+import pytest
+
+@pytest.mark.filterwarnings("ignore")
+def test_match(control: "TestController"): # type: ignore
+    """Runs through the entire span of a practice match"""
+
+    with control.run_robot():
+        # Run disabled
+        control.step_timing(seconds=1, autonomous=True, enabled=False)
+
+        # Run autonomous + enabled
+        control.step_timing(seconds=1, autonomous=True, enabled=True)
+
+        # Disabled for another short period
+        control.step_timing(seconds=0.5, autonomous=False, enabled=False)
+
+        # Run teleop + enabled
+        control.step_timing(seconds=1, autonomous=False, enabled=True)
