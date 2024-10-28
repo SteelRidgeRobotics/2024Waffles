8e79c8e2ffe548e27f828c63ebb81b9f236af819
PickleFace5 <caden.dalley@icloud.com>
2024-10-27 21:34:34 -0700

    e

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -267,7 +267,7 @@ class Drivetrain(Subsystem):
             LimelightHelpers.set_robot_orientation(
                 Constants.Limelight.k_limelight_name,
                 self.gyro.getRotation2d().degrees(),
-                0, # Potentially add -self.navx.getRate() here LATER, according to Chief Delphi it's untested
+                -self.gyro.getRate(), # Potentially add -self.navx.getRate() here LATER, according to Chief Delphi it's untested
                 0, 0, 0, 0
             )
 
